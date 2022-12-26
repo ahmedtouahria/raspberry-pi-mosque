04820b7dac1b51496581cbb871e7b22eaa0cd86a
S-Amine <saidi.amine.p@gmail.com>
2022-12-26 11:35:25 +0100

    create commented example in signals.py for using the mqtt_publisher & put the signals in the correct way

--- adan/apps.py
+++ adan/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class AdanConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'adan'
+    def ready(self):
+        from . import signals
---
+++ adan/signals.py
@@ -0,0 +1,5 @@
+# this is an example for using the publisher
+
+# from mqtt import mqtt_publisher
+
+# mqtt_publisher.main(topic=str(instance.topic), message=json_msg)
